23973c731128cea016d5ce99095812f217cef808
samuel <samuelcontatodev@gmail.com>
2021-09-29 19:41:22 -0300

    craindo carousel usando a API swiper

--- main.js
+++ main.js
@@ -28,3 +28,17 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper', {
+  
+  slidesPerView: 1,
+  
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  mousewheel: true,
+  keyboard: true,
+
+})
