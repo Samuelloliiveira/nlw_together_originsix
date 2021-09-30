9af7ba697c7d255533d3ef090f677fabec8f4b17
samuel <samuelcontatodev@gmail.com>
2021-09-30 11:23:38 -0300

    melhorando algumas animaçoes e arrumando JS

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for(const link of links) {
 }
 
 /* colocar sombra no header quando fazer scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function() {
-  if (this.window.scrollY >= navHeight) {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+  
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   }else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper', {
@@ -58,6 +58,24 @@ scrollReveal.reveal(`
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `, {interval: 100}
 )
+
+/* Botão de voltar ao topo */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  }else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+})
